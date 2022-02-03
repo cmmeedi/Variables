8670c866ea42c8edfb75a0f9140163c9feb9a4f5
cmmeedi <89672577+cmmeedi@users.noreply.github.com>
2022-02-02 20:54:13 -0800

    Add files via upload

---
+++ Variables.rb
@@ -0,0 +1,16 @@
+
+#character_name is assigned
+character_name = "Mike Wizowsky"
+#character_age is assigned
+character_age = "35"
+
+#after 'puts' there needs to be the paranthesis around the sentence where the variables are used
+puts ("There once was a man named " + character_name)
+puts ("he was " + character_age + " years old.")
+
+#I changed the Name and Age of the character
+character_name = "Sullivan"
+character_age = "33"
+
+puts ("He really liked the name " + character_name)
+puts ("but didn't like being " + character_age)
